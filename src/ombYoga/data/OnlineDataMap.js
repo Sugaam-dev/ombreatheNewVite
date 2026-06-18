d6b007aeba149869c0948be898085ca37d3adae3
21051747 <gnimisha41@gmail.com>
2026-06-18 19:46:07 +0530

    2 more pages

--- src/ombYoga/data/OnlineDataMap.js
+++ src/ombYoga/data/OnlineDataMap.js
@@ -3,11 +3,14 @@
 // Mirrors the structure of OmbDataMap.js but for the online YTTC catalog.
 
 import { Online100HoursData } from "../onlinedata/Online100HoursData";
+import { Online300HoursData } from "../onlinedata/Online300HoursData";
+import { Online50HoursPranayamaData } from "../onlinedata/Online50HoursPranayamaData";
 
 export const OnlineDataMap = {
   "100hr": Online100HoursData,
   // "200hr": Online200HoursData,        // add when ready
-  // "300hr": Online300HoursData,        // add when ready
-  // "500hr": Online500HoursData,        // add when ready
+  "300hr": Online300HoursData,
+
+  "500hr": Online50HoursPranayamaData,        // add when ready
   // "kundalini100hr": OnlineKundalini100HoursData,  // add when ready
 };
